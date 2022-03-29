cbe1224466627f2d55e77bd2175426a0e5b13034
Nikhil <Nikhil@HP>
2022-03-29 18:09:32 +0530

    Changes done for Framework

---
+++ Framework/src/main/java/com/abc/commonfunctionality/WaitCondition.java
@@ -0,0 +1,44 @@
+package com.abc.commonfunctionality;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitCondition extends BrowserLaunch {
+	
+	public static FluentWait<WebDriver> wait;
+	
+	public WaitCondition() {
+			wait=new WebDriverWait(driver, 10)
+				.ignoring(TimeoutException.class, NoSuchElementException.class);
+		
+	}
+	
+	public void IsDisplay(final WebElement element) {
+		wait.until(new ExpectedCondition<Boolean>() {
+
+			public Boolean apply(WebDriver input) {
+				// TODO Auto-generated method stub
+				return element.isDisplayed();
+			}
+		});
+	}
+	
+	public void WaitForClickable(WebElement element) {
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+	}
+	
+	public boolean WaitForNavigation(String url) {
+		return wait.until(ExpectedConditions.urlContains(url));
+	}
+	
+	public WebElement isVisible(WebElement webElement, WebDriver driver) {
+	       return wait.until(ExpectedConditions.visibilityOf(webElement));
+	   
+	}
+}
